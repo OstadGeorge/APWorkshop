e7028b0d4e9092df7ea3cb50be5a792a1ad6d6f3
Mohammad Ali Heydari <ostadgeorge@gmail.com>
2020-04-09 01:27:09 +0430

    paint class implemented

---
+++ src/ir/ac/aut/ceit/paint/Paint.java
@@ -0,0 +1,59 @@
+package ir.ac.aut.ceit.paint;
+
+import java.util.ArrayList;
+
+public class Paint {
+    private ArrayList<Shape> shapes;
+
+    public Paint() {
+        this.shapes = new ArrayList<>();
+    }
+
+    public void addShape(Shape shape) {
+        this.shapes.add(shape);
+    }
+
+    public void drawAll() {
+        for(Shape shape : shapes) {
+            if(shape instanceof Circle) {
+                Circle circle = (Circle) shape;
+                System.out.println(circle);
+            } else if(shape instanceof Triangle) {
+                Triangle triangle = (Triangle) shape;
+                System.out.println(triangle);
+            } else {
+                Rectangle rectangle = (Rectangle) shape;
+                System.out.println(rectangle);
+            }
+        }
+    }
+
+    public void printAll() {
+        for(Shape shape : shapes) {
+            if(shape instanceof Circle) {
+                Circle circle = (Circle) shape;
+                System.out.println(circle);
+            } else if(shape instanceof Triangle) {
+                Triangle triangle = (Triangle) shape;
+                System.out.println(triangle);
+            } else {
+                Rectangle rectangle = (Rectangle) shape;
+                System.out.println(rectangle);
+            }
+        }
+    }
+
+    public void describeEqualSides() {
+        for(Shape shape : shapes) {
+            if(shape instanceof Triangle) {
+                Triangle triangle = (Triangle) shape;
+                if(triangle.isEquilateral())
+                    System.out.println(triangle);
+            } else if(shape instanceof Rectangle){
+                Rectangle rectangle = (Rectangle) shape;
+                if(rectangle.isSquare())
+                    System.out.println(rectangle);
+            }
+        }
+    }
+}
