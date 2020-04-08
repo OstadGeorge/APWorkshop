4391f688d636e8a7790193d30056dc03a2230e95
Mohammad Ali Heydari <ostadgeorge@gmail.com>
2020-04-09 00:50:08 +0430

    shape inteface implemented

---
+++ src/ir/ac/aut/ceit/paint/Shape.java
@@ -0,0 +1,13 @@
+package ir.ac.aut.ceit.paint;
+
+/**
+ * Shape interface
+ * @author Mohammad Ali Heydari
+ * @version 0.1
+ */
+public interface Shape {
+    public int calculatePerimeter();
+    public int calculateArea();
+    public int draw();
+    public String toString();
+}
